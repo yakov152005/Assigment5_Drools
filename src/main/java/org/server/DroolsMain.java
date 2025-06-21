5b60b32050b4e4a2be572fd0d13035925ab8fd2a
yakov152005 <yakov152005@gmail.com>
2025-06-21 18:03:50 +0300

    Initial commit

--- src/main/java/org/server/DroolsMain.java
+++ src/main/java/org/server/DroolsMain.java
@@ -1,2 +1,45 @@
-package org.server;public class DroolsMain {
+package org.server;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+public class DroolsMain {
+    public static void main(String[] args) {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kc = ks.getKieClasspathContainer();
+
+        KieSession kSession = kc.newKieSession("ksession-rules");
+        if (kSession == null) {
+            System.err.println(" kSession is null!");
+            return;
+        }
+
+        Person john = new Person("John", 25);
+        Person alice = new Person("Alice", 5);
+        Person baby = new Person("Tiny", 2);
+        Person older = new Person("Sarah", 72);
+
+        kSession.insert(john);
+        kSession.insert(alice);
+        kSession.insert(baby);
+        kSession.insert(older);
+
+        System.out.println(" Starting Drools rule engine...");
+        kSession.fireAllRules();
+        kSession.dispose();
+
+        printPersonStatus(john);
+        printPersonStatus(alice);
+        printPersonStatus(baby);
+        printPersonStatus(older);
+    }
+
+    private static void printPersonStatus(Person p) {
+        System.out.println("------------");
+        System.out.println("Name: " + p.getName());
+        System.out.println("Age: " + p.getAge());
+        System.out.println("Is adult? " + p.isAdult());
+        System.out.println("Is named John? " + p.getIsName());
+    }
 }
--- src/main/java/org/server/Person.java
+++ src/main/java/org/server/Person.java
@@ -1,2 +1,22 @@
-package org.server;public class Person {
+package org.server;
+
+public class Person {
+    private String name;
+    private int age;
+    private boolean isAdult;
+    private boolean isName;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.isAdult = false;
+        this.isName = false;
+    }
+
+    public boolean getIsName() {return isName;}
+    public void setIsName(boolean isName) {this.isName = isName;}
+    public String getName() {return name; }
+    public int getAge() { return age; }
+    public boolean isAdult() { return isAdult; }
+    public void setAdult(boolean isAdult) { this.isAdult = isAdult; }
 }
